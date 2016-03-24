a4c9184e42dd20ca002e260a4a4f8a36af70ea58
Булат <mazikazik64@gmail.com>
2016-03-24 23:37:41 +0400

    Задание 11

---
+++ Task11/Dal/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    public class DALNorthwind
+    {
+        string ConnectionString = "";
+    
+        public IEnumerable<Entities.Order> GetAll()
+        {
+            var result = new List<Entities.Order>();
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                var command = new SqlCommand("SELECT OrderID,OrderDate,ShippedDate from Orders");
+
+                connection.Open();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var order = new Entities.Order
+                            (
+                                reader.GetInt32(0),
+                                reader.GetDateTime(1),
+                                reader.GetDateTime(2)
+                            );
+                        order.SetStatus();
+
+                        result.Add(order);
+                    }
+                }
+
+                return result;
+            }
+        }
+    }
+    }
---
+++ Task11/Entities/Order.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public char CustomerID { get; set; }
+        public char EmployeeID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime RequiredDate { get; set; }
+        public DateTime ShippedDate { get; set; }
+        public int ShipVia { get; set; }
+        public double Freight { get; set; }
+        public char ShipName { get; set; }
+        public char ShipAddress { get; set; }
+        public char ShipCity { get; set; }
+        public char ShipRegion { get; set; }
+        public char ShipPostalCode { get; set; }
+        public char ShipCountry { get; set; }
+        private enum Enum { New = 1, InWork, Executed };
+        public int status;
+
+        public Order(int id, DateTime OrDate, DateTime ShipDate)
+        {
+            OrderID = id;
+            OrderDate = OrDate;
+            ShippedDate = ShippedDate;
+            status = (int)Enum.Executed;
+        }
+
+        public void SetStatus()
+        {
+            if (OrderDate == null && ShippedDate == null)
+            {
+                status = (int)Enum.New;
+            }
+            else if (ShippedDate == null && OrderDate != null)
+            {
+                status = (int)Enum.InWork;
+            }
+            else
+            {
+                status = (int)Enum.Executed;
+            }
+        }
+
+        }
+}
